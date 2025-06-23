ce4f7ffa712306b3470ff2788afd51ef04a0b6f2
Hugo Ferrando Seage <hugoseage@gmail.com>
2025-06-23 09:56:10 +0200

    OS specific folders with some setup files

---
+++ windows/install.ps1
@@ -0,0 +1,9 @@
+winget install M2Team.NanaZip
+winget install Apple.iTunes
+winget install Blizzard.BattleNet
+winget install Discord.Discord
+winget install Microsoft.Teams
+winget install Microsoft.VisualStudioCode.User-x64
+winget install Microsoft.WindowsTerminal
+winget install PrivateInternetAccess.PrivateInternetAccess
+winget install Valve.Steam
